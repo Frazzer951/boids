e7b2730f3c63521b662e39aca6654db5785aca0e
Luke Eltiste <luke343279@gmail.com>
2022-06-04 12:38:14 -0700

    add 3 rules

--- src/components.rs
+++ src/components.rs
@@ -6,6 +6,17 @@ pub struct Velocity {
     pub x: f32,
     pub y: f32,
 }
+impl Velocity {
+    pub fn magnitude(&self) -> f32 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+    pub fn normalize(&mut self) -> &mut Self {
+        let mag = self.magnitude();
+        self.x /= mag;
+        self.y /= mag;
+        self
+    }
+}
 
 #[derive(Component)]
 pub struct Movable;
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,26 @@ use std::f32::consts::PI;
 use bevy::math::{vec2, vec3};
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
-use rand::{Rng, thread_rng};
+use rand::{thread_rng, Rng};
 
 use crate::components::{Boid, Movable, Velocity};
 
 mod components;
 
+// region - Game Settings
+const BOID_COHERENCE: f32 = 2.;
+const BOID_SEPARATION: f32 = 40.;
+const SEPARATION_DISTANCE: f32 = 30.;
+const BOID_ALIGNMENT: f32 = 60.;
+// endregion
+
 // region - Game Constants
 const BOID_ROTATE_OFFSET: f32 = -PI / 2.;
-const BOID_SCALE: f32 = 10.;
-const BOID_SPEED: f32 = 200.;
-const NUMBER_OF_BOIDS: usize = 10;
+const BOID_SCALE: f32 = 5.;
+const BOID_BASE_SPEED: f32 = 200.;
+const BOID_MIN_SPEED: f32 = 100.;
+const BOID_MAX_SPEED: f32 = 500.;
+const NUMBER_OF_BOIDS: usize = 200;
 // endregion
 
 // region - Resources
@@ -33,6 +42,8 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
+        .add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
+        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup_system)
         .add_startup_system_to_stage(StartupStage::PostStartup, boid_spawn_system)
         .add_system(movable_system)
@@ -89,8 +100,8 @@ fn boid_spawn_system(mut commands: Commands, win_size: Res<WinSize>) {
         spawn_boid(
             rng.gen_range(-win_size.w..win_size.w) as f32,
             rng.gen_range(-win_size.h..win_size.h) as f32,
-            angle.cos() * BOID_SPEED,
-            angle.sin() * BOID_SPEED,
+            angle.cos() * BOID_BASE_SPEED,
+            angle.sin() * BOID_BASE_SPEED,
         );
     }
 }
@@ -106,12 +117,12 @@ fn movable_system(
         let x = velocity.x * time_delta;
         let y = velocity.y * time_delta;
         let angle = (if velocity.x == 0. {
-            0.
+            0_f32
         } else {
             velocity.y / velocity.x
         })
-        .atan() + if velocity.x < 0. {PI} else {0.};
-
+        .atan()
+            + if velocity.x < 0. { PI } else { 0. };
 
         // Update position
         let translation = &mut transform.translation;
@@ -121,47 +132,74 @@ fn movable_system(
         translation.x = wrap(translation.x, -win_size.w / 2., win_size.w / 2.);
         translation.y = wrap(translation.y, -win_size.h / 2., win_size.h / 2.);
 
-        println!(
-            "x:{}, y:{}, angle:{}",
-            velocity.x,
-            velocity.y,
-            angle * (180. / PI)
-        );
-
         transform.rotation = Quat::from_rotation_z(angle + BOID_ROTATE_OFFSET);
     }
 }
 
 fn update_boid_system(
     time: Res<Time>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Velocity, With<Boid>>,
+    mut query: Query<(&mut Velocity, &mut Transform), With<Boid>>,
 ) {
     let time_delta = time.delta_seconds();
-    let rotation_speed = PI / 2.;
+    let positions: Vec<(f32, f32)> = query
+        .iter()
+        .map(|(_vel, tf)| (tf.translation.x, tf.translation.y))
+        .collect();
+    let sum_positions: (f32, f32) = positions
+        .iter()
+        .fold((0., 0.), |acc, (x, y)| (acc.0 + x, acc.1 + y));
+    let sum_velocities = query
+        .iter()
+        .fold((0., 0.), |acc, (vel, _tf)| (acc.0 + vel.x, acc.1 + vel.y));
 
-    let rotation_delta = if keyboard_input.pressed(KeyCode::A) {
-        -rotation_speed * time_delta
-    } else if keyboard_input.pressed(KeyCode::D) {
-        rotation_speed * time_delta
-    } else {
-        0.
-    };
+    for (mut velocity, tf) in query.iter_mut() {
+        let translation = tf.translation;
+        // rule 1 -- coherence - center of mass
+        let sum_positions = (
+            (sum_positions.0 - translation.x) / (NUMBER_OF_BOIDS as f32 - 1.),
+            (sum_positions.1 - translation.y) / (NUMBER_OF_BOIDS as f32 - 1.),
+        );
+        let r1_vec = (
+            BOID_COHERENCE * (sum_positions.0 - translation.x) / 100.,
+            BOID_COHERENCE * (sum_positions.1 - translation.y) / 100.,
+        );
 
-    for mut velocity in query.iter_mut() {
-        let rotation = rotate(rotation_delta, velocity.x, velocity.y);
-        velocity.x = rotation.0;
-        velocity.y = rotation.1;
+        // rule 2 -- separation - keep distance
+        let mut r2_vec = (0., 0.);
+        let cur_pos = &(translation.x, translation.y);
+        for pos in &positions {
+            if pos != cur_pos && distance(cur_pos, pos) < SEPARATION_DISTANCE {
+                let x_diff = pos.0 - cur_pos.0;
+                let y_diff = pos.1 - cur_pos.1;
+                r2_vec.0 -= x_diff;
+                r2_vec.1 -= y_diff;
+            }
+        }
+        r2_vec = (
+            BOID_SEPARATION * r2_vec.0 / 100.,
+            BOID_SEPARATION * r2_vec.1 / 100.,
+        );
+
+        // rule 3 -- alignment - match velocities
+        let sum_velocities = (
+            (sum_velocities.0 - velocity.x) / (NUMBER_OF_BOIDS as f32 - 1.),
+            (sum_velocities.1 - velocity.y) / (NUMBER_OF_BOIDS as f32 - 1.),
+        );
+        let r3_vec = (
+            BOID_ALIGNMENT * (sum_velocities.0 - velocity.x) / 100.,
+            BOID_ALIGNMENT * (sum_velocities.1 - velocity.y) / 100.,
+        );
+
+        // update velocity
+        velocity.x += r1_vec.0 + r2_vec.0 + r3_vec.0 * time_delta;
+        velocity.y += r1_vec.1 + r2_vec.1 + r3_vec.1 * time_delta;
+
+        // normalize velocities
+        let boid_speed = velocity.magnitude().max(BOID_MIN_SPEED).min(BOID_MAX_SPEED);
+        velocity.normalize();
+        velocity.x *= boid_speed;
+        velocity.y *= boid_speed;
     }
-}
-
-fn rotate(angle: f32, x: f32, y: f32) -> (f32, f32) {
-    let s = angle.sin();
-    let c = angle.cos();
-
-    let x_prime = x * c + y * s;
-    let y_prime = -x * s + y * c;
-    (x_prime, y_prime)
 }
 
 fn wrap(val: f32, min: f32, max: f32) -> f32 {
@@ -172,4 +210,10 @@ fn wrap(val: f32, min: f32, max: f32) -> f32 {
     } else {
         val
     }
+}
+
+fn distance(p1: &(f32, f32), p2: &(f32, f32)) -> f32 {
+    let dx = p2.0 - p1.0;
+    let dy = p2.1 - p1.1;
+    (dx * dx + dy * dy).sqrt()
 }
